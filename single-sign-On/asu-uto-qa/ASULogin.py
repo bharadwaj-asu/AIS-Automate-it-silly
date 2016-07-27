f63603b5ad1a5100c3459e268a1e323b7df75a6f
bhaddy <baryasom@asu.edu>
2016-07-27 09:13:41 -0700

    Single-sign-On-ASU

---
+++ single-sign-On/asu-uto-qa/ASULogin.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-t
+from selenium import webdriver
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException
+from _codecs import decode
+class ASULoginPythonScript:
+
+    def test_a_s_u_login_python_script(self,username,password,url):
+        print("reached here")
+        self.driver = webdriver.Firefox()
+        driver = self.driver
+        driver.get(url)
+
+        driver.find_element_by_id("asu_hdr_ssi").click()
+        driver.find_element_by_id("username").clear()
+        driver.find_element_by_id("username").send_keys(username)
+        driver.find_element_by_id("password").clear()
+        driver.find_element_by_id("password").send_keys(password)
+        driver.find_element_by_css_selector("input.submit").click()
+        cookies = driver.get_cookies()
+        print cookies
+
---
+++ single-sign-On/asu-uto-qa/user_input.py
@@ -0,0 +1,24 @@
+
+#this is to get all the credentials of the user and user website
+import getpass
+#this makes your password not appear on the screen
+from ASULogin import  ASULoginPythonScript
+class User_input:
+    def userprofile(self):
+        self.username = raw_input("Enter your username")
+        self.password=getpass.getpass(prompt="Enter you ASURITE Password")
+        self.url=raw_input("Enter the Url")
+
+    def connect_to_url(self):
+        x=ASULoginPythonScript()
+        x.test_a_s_u_login_python_script(self.username,self.password,self.url)
+        print("done")
+
+y=User_input()
+y.userprofile()
+y.connect_to_url()
+
+
+
+
+
